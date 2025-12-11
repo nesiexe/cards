54e2d77bb5d4542d22d2f9f6d7acdcb7d5c88d38
Nesi <p.wozniak.2006@gmail.com>
2025-12-11 20:41:51 +0100

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,30 +1,54 @@
+function nodeFactory(node) {
+  return function(children, props) {
+    if(!Array.isArray(children)) children = [children];
+    const el = document.createElement(node);
+    if (props) Object.assign(el, props);
+    for(const child of children) {
+      if(!child) continue;
+      el.appendChild(typeof child === "object" ? child : document.createTextNode("" + child));
+    }
+    return el;
+  }
+}
+
+const div = nodeFactory("div");
+const link = nodeFactory("a");
+
+function clearEl(el) {
+  el.textContent = "";
+}
+
 async function updateNowPlaying() {
   const res = await fetch("/api/now-playing");
   const data = await res.json();
   const el = document.getElementById("now-playing");
 
   if (data.isPlaying) {
+    clearEl(el);
 
-    el.innerHTML = `
-      <div>
-        <div>I'm listening to</div>
-        <a class="now-playing-green" href="${data.url}">${data.artist} - ${data.track}</a>
-        <div>right now on spotify :3</div>
-      </div>
-    `;
+    el.appendChild(
+      div([
+        div("I'm listening to"),
+        link(`${data.artist} - ${data.track}`, {
+          className: "now-playing-green",
+          href: data.url
+        }),
+        div("right now on spotify :3")
+      ])
+    );
   } else {
-    el.innerHTML = `Im not listening to anything right now :c`;
+    el.textContent = `Im not listening to anything right now :c`;
   }
 }
 
 async function fetchPfp() {
   const p = document.getElementById("pfp");
 
-  p.addEventListener("click", ()=> {
-  p.classList.add("rot");
-  setTimeout(()=> {
-    p.classList.remove("rot");
-  }, 1000);
+  p.addEventListener("click", () => {
+    p.classList.add("rot");
+    setTimeout(() => {
+      p.classList.remove("rot");
+    }, 1000);
   });
 }
 
